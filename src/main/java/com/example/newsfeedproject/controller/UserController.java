d568834d63212477f7a70d30b7bac27a6eebc0d0
Jeon <jeongsh950518@gmail.com>
2023-11-22 16:29:09 +0900

    로그인 구현 그리고 수정

--- src/main/java/com/example/newsfeedproject/controller/UserController.java
+++ src/main/java/com/example/newsfeedproject/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.example.newsfeedproject.controller;
 
+import com.example.newsfeedproject.dto.LoginRequestDto;
 import com.example.newsfeedproject.dto.SignupRequestDto;
+import com.example.newsfeedproject.dto.StatusDto;
 import com.example.newsfeedproject.repository.UserRepository;
 import com.example.newsfeedproject.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class UserController {
 
@@ -19,14 +20,20 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/signup")
-    public void signupPage() {
-        return; //리턴 페이지 수정해야함!!! 회원가입 페이지 반환하기
-    }
+//    @GetMapping("/user/signup")
+//    public void signupPage() {
+//        return; //리턴 페이지 수정해야함!!! 회원가입 페이지 반환하기
+//    }
 
     @PostMapping("/user/signup")
-    public void signup(@RequestBody SignupRequestDto requestDto) {
-        userService.signup(requestDto);
-        return ; //로그인 페이지 반환하기!!!
+    public ResponseEntity<StatusDto> signup(@RequestBody SignupRequestDto requestDto) {
+        return userService.signup(requestDto);
     }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<StatusDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse res){
+        return userService.login(loginRequestDto,res);
+    }
+
+
 }
